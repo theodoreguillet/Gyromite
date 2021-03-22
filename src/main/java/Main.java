9927a6a4c51e13088af36d064805e33164174733
noureddine-m-kanaan <noureddine.m.kanaan@gmail.com>
2021-03-22 19:40:43 +0100

    InputV1, InputV2 et Resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
+import scene.Input_v1;
+import scene.Resources;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame testJ = new JFrame();
+        testJ.setVisible(true);
+
+        // Tests of Resources class
+        /*Resources resources = new Resources();
+        String path = "src/img/test.jpg";
+        String id = "test";
+        resources.loadImage(path, id);
+
+        JLabel photo = new JLabel();
+        BufferedImage imgTest;
+        imgTest = (BufferedImage) resources.getImage("test");
+        photo.setIcon(new ImageIcon(imgTest));
+        testJ.getContentPane().add(photo);*/
+
+       // Input_v2 in = new Input_v2();
+
+    }
+
 }
---
+++ src/main/java/scene/Resources.java
@@ -0,0 +1,33 @@
+package scene;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Resources {
+    private Map<String, BufferedImage> images;
+
+    public Resources() {
+        images = new HashMap<>();
+    }
+
+    public void loadImage(String path, String id) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(path));
+            images.put(id, img);
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public BufferedImage getImage(String id) {
+        return images.get(id);
+    }
+}
