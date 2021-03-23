ff21e24bc27a9e85da7af95eb52974e037bb3698
Watchtux <guillettheodore@gmx.com>
2021-03-23 20:31:50 +0100

    Input unit test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import game.Game;
-import game.SceneTestGame;
-import game.SceneTestInput;
 import scene.Scene;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World !!");
-        Scene game = new SceneTestInput();
+        Scene game = new Game();
         game.start();
     }
 }
