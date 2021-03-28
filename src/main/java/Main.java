28908c245f0e22912812cb3e0e17d9b7e6383441
Watchtux <guillettheodore@gmx.com>
2021-03-29 00:59:50 +0200

    comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import scene.Scene;
 
 public class Main {
     public static void main(String[] args) {
-        Scene game = new SceneTestGame();
+        Scene game = new SceneTestPhysics();
         game.start();
     }
 }
--- src/main/java/scene/Node.java
+++ src/main/java/scene/Node.java
@@ -9,54 +9,103 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
+/**
+ * Base class for all {@link Scene} objects.
+ * The nodes are organised in a tree belonging to the scene.
+ * Each node has a parent (except the {@link SceneRoot} and any number of children.
+ */
 public class Node {
     private final ArrayList<Node> children = new ArrayList<>();
     private Node owner = null;
     private Body body = null;
 
+    private final Vector2 position = new Vector2();
+    private double orient = 0.0; // Orientation in radians
+
     private boolean updatingChildNodes = false;
     private final ArrayList<Node> childrenToRemove = new ArrayList<>();
 
-    private Vector2 position = new Vector2();
-    private double orient = 0.0; // Orientation in radians
-
     public Node() { }
 
+    /**
+     * @return Return true if the node is inside a {@link Scene}. A node is connected to
+     *         the scene tree by his parent until the root node (see {@link SceneRoot}).
+     */
     public boolean isInScene() {
         return scene() != null;
     }
+    /**
+     * @return Return true if the node has a physics body.
+     */
     public boolean hasBody() {
         return body != null;
     }
-
+    /**
+     * @return The {@link Scene} object on which the node belong or null if the
+     *         node is not connected to the scene tree (see {@link Node#isInScene()}).
+     */
     public Scene scene() {
         return owner == null ? null : owner.scene();
     }
+    /**
+     * @return The parent of this node
+     */
     public Node owner() {
         return owner;
     }
+    /**
+     * @return The physics body of this node
+     */
     public Body body() {
         return body;
     }
+
+    /**
+     * @return Relative position of the node
+     */
     public Vector2 position() {
         return position;
     }
+    /**
+     * @return Relative orientation of the node
+     */
     public double orient() {
         return orient;
     }
+    /**
+     * Compute the absolute position of the node in the world
+     * @return Absolute position of the node
+     */
     public Vector2 worldPosition() {
         return owner != null ? position().clone().rotate(owner.worldOrient()).addi(owner.worldPosition()) : position();
     }
+    /**
+     * Compute the absolute orientation of the node in the world
+     * @return Absolute orientation of the node
+     */
     public double worldOrient() {
         return owner != null ? owner.worldOrient() + orient() : orient();
     }
 
+    /**
+     * Set the relative position of the node
+     * @param x The x coordinate
+     * @param y The y coordinate
+     */
     public void setPosition(double x, double y) {
         position.set(x, y);
     }
+    /**
+     * Set the relative position of the node
+     * @param pos The coordinates
+     */
     public void setPosition(Vector2 pos) {
         position.set(pos);
     }
+    /**
+     * Set the relative orientation of the node
+     * @param orient The orientation in radian
+     */
     public void setOrient(double orient) {
         this.orient = orient;
     }
@@ -100,6 +149,7 @@ public class Node {
     public void removeChild(Node childNode) {
         if(children.contains(childNode)) {
             childNode.removeBody();
+
             if(updatingChildNodes) {
                 childrenToRemove.add(childNode);
             } else {
@@ -145,10 +195,23 @@ public class Node {
      */
     protected void init() { }
 
+    /**
+     * Destroy method called when {@link Node#remove()} or {@link Node#removeChild(Node)} is called with this node.
+     * This method should be overrided to free resources and allow this object to be garbage collected.
+     */
+    protected void destroy() { }
+
+    /**
+     * Update method called each update tick (See {@link core.MainLoop})
+     */
     protected void update() {
         updateChildNodes();
     }
 
+    /**
+     * Render method called each render frame (See {@link core.MainLoop})
+     * @param g The graphics context
+     */
     protected void render(Graphics2D g) {
         AffineTransform at = new AffineTransform(g.getTransform());
 
