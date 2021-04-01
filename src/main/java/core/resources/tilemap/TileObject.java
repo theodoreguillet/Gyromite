b2c3808a53ebfc8b8c3af5432583cd06d35c07ed
Watchtux <guillettheodore@gmx.com>
2021-04-01 17:56:57 +0200

    double tile object coords

--- src/main/java/core/resources/tilemap/TileObject.java
+++ src/main/java/core/resources/tilemap/TileObject.java
@@ -8,8 +8,8 @@ public class TileObject {
     public int id;
     public String name = "";
     public int gid = 0; // Tile id
-    public int x, y;
-    public int width, height;
+    public double x, y;
+    public double width, height;
     public String type = "";
     public double rotation = 0.0;
     public boolean visible = true;
